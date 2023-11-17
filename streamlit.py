0a848c259ad706d91adc52a7c964b42efebefc5c
stonesy88 <41800496+stonesy88@users.noreply.github.com>
2023-11-17 09:33:38 +0000

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,50 +1,20 @@
 import streamlit as st
-from pyvis.network import Network
-import pandas as pd
-import networkx as nx
 
-# Define the Pyvis network graph
-def create_network_graph():
-    net = Network(height="600px", width="100%", bgcolor="#222222", font_color="white")
-    # This is where you'd add nodes and edges for your actual Pyvis graph
-    # net.add_node(...)
-    # net.add_edge(...)
-    return net
-
-# Layout for the title and the image
+# Layout for the title and the logo
 col1, col2 = st.columns([3, 1])
 with col1:
     st.title('Single Customer View')
 with col2:
+    # Direct link to the image
     st.image('https://upload.wikimedia.org/wikipedia/commons/0/01/New-betfred-logo.png')
 
-# Sidebar for filters
-with st.sidebar:
-    st.header('Filters')
-    # Create filter options as per the mockup
-    matched_customers = st.multiselect('Matched Retail Customers', options=['Customer 1', 'Customer 2'], default=['Customer 1'])
-    store_id = st.selectbox('Store ID', options=['Store 1', 'Store 2'], index=0)
-    bet_pattern = st.radio('Bet Pattern', options=['Pattern 1', 'Pattern 2'])
-    external_exclusions = st.checkbox('External Exclusions', value=True)
-    bf_member = st.checkbox('BF Member', value=True)
+# Rest of your Streamlit code
+# ...
 
-# Create tabs for different views
-tab1, tab2 = st.tabs(["SCV Graph Analysis", "Monitored Customers"])
-
-# Tab for the SCV Graph Analysis - Place the placeholder image here
+# Tab for the SCV Graph Analysis - Replace with the correct direct URL to the image
 with tab1:
-    # If you had a function to display the Pyvis graph, it would go here
-    # For now, we'll display the placeholder image
-    placeholder_image = 'https://pyvis.readthedocs.io/en/latest/_images/net.png'  # Update with the path to your image
-    st.image(placeholder_image, caption='Network Graph Placeholder')
+    # Placeholder image URL
+    placeholder_image_url = 'https://pyvis.readthedocs.io/en/latest/_images/net.png'  # Replace with the direct URL to the image
+    st.image(placeholder_image_url, caption='Network Graph Placeholder')
 
-# Tab for Monitored Customers - This section would contain additional information
-# or controls pertaining to monitored customers as per your application requirements
-with tab2:
-    st.write("Details for Monitored Customers")
-
-# Legend - You can create a static legend or dynamically update it based on the graph
-st.sidebar.header('Legend')
-st.sidebar.markdown('🔵 - Stores')
-st.sidebar.markdown('🟢 - Observation Traits')
-st.sidebar.markdown('🟠 - BF Member')
+# ...
